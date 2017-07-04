b454fec33a75b0f12384628593c96688182ec052
George Li <george53@bu.ed>
2017-07-04 14:43:46 -0400

    missed a save

--- app/controllers/api/v1/inventories_controller.rb
+++ app/controllers/api/v1/inventories_controller.rb
@@ -2,6 +2,15 @@ class Api::V1::InventoriesController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
-
+    body=request.body.read
+    parsed=JSON.parse(body)
+    inventory=Inventory.new(parsed)
+    character.user_id = current_user.id
+    character.name = current_user.name
+    if character.save
+      render json: { message: ["It worked!"]}
+    else
+      render json: { message: character.errors.full_messages}
+    end
   end
 end
